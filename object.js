9b9aa0265ac0e24c43c64dbd304453a82698960f
Neve-06 <6255234@rts-365.dk>
2025-02-03 13:44:06 +0100

    added block notations

--- object.js
+++ object.js
@@ -1,8 +1,14 @@
 let person = {
     firstname: "Tim",
     lastname: "Pyke",
-    age: 48
+    age: 48,
+    hometown: "Roskilde",
+    pet: "spooder",
+    petname: "man"
 }
 
 console.log(person.firstname);
 console.log(person.lastname);
+
+console.log(person["pet"]);
+console.log(person["petname"]);

c070c9997222fd6061ccc4fbc231db911cf98523
Neve-06 <6255234@rts-365.dk>
2025-02-04 08:44:48 +0100

    udkommenter text, object.js

--- object.js
+++ object.js
@@ -1,16 +1,16 @@
-let person = {
-    firstname: "Tim",
-    lastname: "Pyke",
-    married: "true",
-    age: 48,
-    hometown: "Roskilde",
-    hobbies: ["history", "writing", "programming" ],
-    pet:{
-        type: "spooder",
-        name: "man"
-    },
+// let person = {
+//     firstname: "Tim",
+//     lastname: "Pyke",
+//     married: "true",
+//     age: 48,
+//     hometown: "Roskilde",
+//     hobbies: ["history", "writing", "programming" ],
+//     pet:{
+//         type: "spooder",
+//         name: "man"
+//     },
     
-}
+// }
 
 // console.log(person.firstname);
 // console.log(person.lastname);
@@ -19,10 +19,10 @@ let person = {
 // console.log(person["petname"]);
 
 
-person.hobbies.forEach(function(hobby){
-    console.log(hobby);
+// person.hobbies.forEach(function(hobby){
+//     console.log(hobby);
     
-})
+// })
 
-console.log(person.pet.type);
-console.log(person.pet.name);
+// console.log(person.pet.type);
+// console.log(person.pet.name);
